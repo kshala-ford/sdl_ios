f9d3929905c3b951bd0a7f3274f3be3b84f82506
Satbir Tanda <satbirtanda@gmail.com>
2020-04-14 17:25:01 -0700

    Update SDLPttbControlCapabilities.h

--- SmartDeviceLink/SDLPttbControlCapabilities.h
+++ SmartDeviceLink/SDLPttbControlCapabilities.h
@@ -13,7 +13,85 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+* Contains information about a pttb control module's capabilities.
+*/
 @interface SDLPttbControlCapabilities : NSObject
+
+/**
+ *
+ * Max string length 100 chars
+ *
+ * Required
+ */
+@property (strong, nonatomic) NSString *moduleName;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *stateAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *resetAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *faltMsgAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *engOnMsgAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *loFuelMsgAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *powerStatusAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *outletAAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *outletBAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *pwMaxAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *hwConfigAvailable;
+
+/**
+ *
+ * Optional, Boolean
+ */
+@property (nullable, strong, nonatomic) NSNumber<SDLBool> *pwConsumptionAvailable;
+
 
 @end
 
