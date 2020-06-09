36981198f139e5bb0261ec512407d17b7797b739
Satbir Tanda <satbirtanda@gmail.com>
2020-06-09 08:46:35 -0700

    Add SDLObsshDisplayUnit

---
+++ SmartDeviceLink/SDLObsshDisplayUnit.m
@@ -0,0 +1,12 @@
+//
+//  SDLObsshDisplayUnit.m
+//  SmartDeviceLink
+//
+//  Created by Tanda, Satbir (S.S.) on 6/9/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import "SDLObsshDisplayUnit.h"
+
+SDLObsshDisplayUnit const SDLObsshDisplayUnitKg = @"KG";
+SDLObsshDisplayUnit const SDLObsshDisplayUnitLb = @"LB";
