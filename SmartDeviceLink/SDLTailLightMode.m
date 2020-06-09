541292132fc6bc409014da80554e5ce4505ebad4
Satbir Tanda <satbirtanda@gmail.com>
2020-06-09 08:46:53 -0700

    Add SDLTailLightMode

---
+++ SmartDeviceLink/SDLTailLightMode.m
@@ -0,0 +1,14 @@
+//
+//  SDLTailLightMode.m
+//  SmartDeviceLink
+//
+//  Created by Tanda, Satbir (S.S.) on 6/9/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import "SDLTailLightMode.h"
+
+SDLTailLightMode const SDLTailLightModeActiveObs = @"INACTIVE";
+SDLTailLightMode const SDLTailLightModeActiveSmt = @"OBS_ACTIVE";
+SDLTailLightMode const SDLTailLightModeActiveWdh = @"ADM_ACTIVE";
+SDLTailLightMode const SDLTailLightModeDeactiveAll = @"WDH_ACTIVE";
