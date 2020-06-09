879caa09f973c351fc3d1e7968581267746347ad
Satbir Tanda <satbirtanda@gmail.com>
2020-06-09 08:46:45 -0700

    Add SDLScreenMode

---
+++ SmartDeviceLink/SDLScreenMode.m
@@ -0,0 +1,14 @@
+//
+//  SDLScreenMode.m
+//  SmartDeviceLink
+//
+//  Created by Tanda, Satbir (S.S.) on 6/9/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import "SDLScreenMode.h"
+
+SDLScreenMode const SDLScreenModeInactive = @"INACTIVE";
+SDLScreenMode const SDLScreenModeObsActive = @"OBS_ACTIVE";
+SDLScreenMode const SDLScreenModeAdmActive = @"ADM_ACTIVE";
+SDLScreenMode const SDLScreenModeWdhActive = @"WDH_ACTIVE";
