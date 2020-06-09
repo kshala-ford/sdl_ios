f0150c5483435ba54ebaf896a007f631d449eedb
Satbir Tanda <satbirtanda@gmail.com>
2020-06-09 08:46:27 -0700

    Add SDLFrontAxleLoadRestorationStatus

---
+++ SmartDeviceLink/SDLFrontAxleLoadRestorationStatus.m
@@ -0,0 +1,18 @@
+//
+//  SDLFrontAxleLoadRestorationStatus.m
+//  SmartDeviceLink
+//
+//  Created by Tanda, Satbir (S.S.) on 6/9/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import "SDLFrontAxleLoadRestorationStatus.h"
+
+SDLFrontAxleLoadRestorationStatus const SDLFrontAxleLoadRestorationStatusInactive = @"INACTIVE";
+SDLFrontAxleLoadRestorationStatus const SDLFrontAxleLoadRestorationStatusFirstMeasurementCollected = @"FIRST_MEASUREMENT_COLLECTED";
+SDLFrontAxleLoadRestorationStatus const SDLFrontAxleLoadRestorationStatusSecondMeasurementCollected = @"SECOND_MEASURMENT_COLLECTED";
+SDLFrontAxleLoadRestorationStatus const SDLFrontAxleLoadRestorationStatusHeightCorrectionCompleted = @"HEIGHT_CORRECTION_COMPLETE";
+SDLFrontAxleLoadRestorationStatus const SDLFrontAxleLoadRestorationStatusNotUsed1 = @"NOT_USED_1";
+SDLFrontAxleLoadRestorationStatus const SDLFrontAxleLoadRestorationStatusNotUsed2 = @"NOT_USED_2";
+SDLFrontAxleLoadRestorationStatus const SDLFrontAxleLoadRestorationStatusNotUsed3 = @"NOT_USED_3";
+SDLFrontAxleLoadRestorationStatus const SDLFrontAxleLoadRestorationStatusFaulty = @"FAULTY";
