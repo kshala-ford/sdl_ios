1823d9162fa6b7b1c2a6e19faf912b6beaef8260
Satbir Tanda <satbirtanda@gmail.com>
2020-06-12 02:31:04 -0700

    Add SDLFrontAxleLoadRestorationSpec

---
+++ SmartDeviceLinkTests/RPCSpecs/EnumSpecs/SDLFrontAxleLoadRestorationSpec.m
@@ -0,0 +1,27 @@
+//
+//  SDLFrontAxleLoadRestorationSpec.m
+//  SmartDeviceLinkTests
+//
+//  Created by Tanda, Satbir (S.S.) on 6/12/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Quick/Quick.h>
+#import <Nimble/Nimble.h>
+
+#import "SDLFrontAxleLoadRestoration.h"
+
+QuickSpecBegin(SDLFrontAxleLoadRestorationSpec)
+
+describe(@"Individual Enum Value Tests", ^ {
+    it(@"Should match internal values", ^ {
+        expect(SDLFrontAxleLoadRestorationExit).to(equal(@"EXIT"));
+        expect(SDLFrontAxleLoadRestorationRequestFirstMeasurement).to(equal(@"REQUEST_FIRST_MEASUREMENT"));
+        expect(SDLFrontAxleLoadRestorationRequestSecondMeasurement).to(equal(@"REQUEST_SECOND_MEASUREMENT"));
+        expect(SDLFrontAxleLoadRestorationComplete).to(equal(@"COMPLETE"));
+    });
+});
+
+QuickSpecEnd
