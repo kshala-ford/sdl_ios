75c347229895115d428d0a88fedcc0297f4a19b7
Satbir Tanda <satbirtanda@gmail.com>
2020-06-12 02:31:57 -0700

    Add SDLTailLightModeSpec

---
+++ SmartDeviceLinkTests/RPCSpecs/EnumSpecs/SDLTailLightModeSpec.m
@@ -0,0 +1,27 @@
+//
+//  SDLTailLightModeSpec.m
+//  SmartDeviceLinkTests
+//
+//  Created by Tanda, Satbir (S.S.) on 6/12/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Quick/Quick.h>
+#import <Nimble/Nimble.h>
+
+#import "SDLTailLightMode.h"
+
+QuickSpecBegin(SDLTailLightModeSpec)
+
+describe(@"Individual Enum Value Tests", ^ {
+    it(@"Should match internal values", ^ {
+        expect(SDLTailLightModeActiveObs).to(equal(@"ACTIVATE_OBS"));
+        expect(SDLTailLightModeActiveSmt).to(equal(@"ACTIVATE_SMT"));
+        expect(SDLTailLightModeActiveWdh).to(equal(@"ACTIVATE_WDH"));
+        expect(SDLTailLightModeDeactiveAll).to(equal(@"DEACTIVATE_ALL"));
+    });
+});
+
+QuickSpecEnd
