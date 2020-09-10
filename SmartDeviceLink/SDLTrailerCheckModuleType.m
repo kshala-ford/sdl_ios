07cfbaed1415fc6740d2527681f75b4b99035dfa
Parth Brahmbhatt <pbrahmb3@ford.com>
2020-09-10 17:33:38 -0400

    Added all the enums for trailer check.

---
+++ SmartDeviceLink/SDLTrailerCheckModuleType.m
@@ -0,0 +1,13 @@
+//
+//  SDLTrailerCheckModuleType.m
+//  SmartDeviceLink
+//
+//  Created by Brahmbhatt, Parth (P.) on 9/10/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+//  ACVL_Modified_SDL_File
+
+#import "SDLTrailerCheckModuleType.h"
+
+SDLTrailerCheckModuleType const SDLTrailerCheckModuleTypeTLC = @"TLC";
+
